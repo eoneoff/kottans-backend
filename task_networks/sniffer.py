f33181972fb7396b4de392c64061a0a0c012c14d
eoneoff <eoneoff@gmail.com>
2019-09-25 15:19:53 +0300

    provided sniffer works as executable and module

--- task_networks/sniffer.py
+++ task_networks/sniffer.py
@@ -5,7 +5,7 @@ import socket
 import sys
 import re
 
-def twister():
+def _twister():
     current = '\\'
     while True:
         if current == '\\':
@@ -21,35 +21,47 @@ def twister():
             current = "\\"
             yield current
 
-def parseArguments(args):
+def _parseHost(host):
+    if not re.search('^((25[0-5]|2[0-4]\d|[01]?\d?\d)\.){3}(25[0-5]|2[0-4]\d|[01]?\d?\d)$', host):
+        try:
+            return socket.gethostbyname(host)
+        except socket.gaierror as er:
+            er.args="Hostname could not be resolved"
+            raise er
+    else: return host
+
+def _parsePorts(ports):
+    if not re.search('^\d{1,4}-\d{1,4}$', ports):
+        raise ValueError
+    limits = [int(i) for i in ports.split('-')]
+    if limits[0] >= limits[1] or any([i not in range(1, 65536) for i in limits]):
+        raise ValueError
+    else:
+        return range(limits[0], limits[1]+1)
+
+
+def _parseArguments(args):
     host = ""
     ports = range(1,65536)
     if '--host' not in args or args.index('--host') == len(args)- 1:
         raise ValueError("You must specify a host to scan ports")
     else:
-        host = args[args.index('--host') + 1]
-        if not re.search('^((25[0-5]|2[0-4]\d|[01]?\d?\d)\.){3}(25[0-5]|2[0-4]\d|[01]?\d?\d)$', host):
-            try:
-               host = socket.gethostbyname(host);
-            except socket.gaierror as er:
-                er.args="Hostname could not be resolved"
-                raise er
+        host = _parseHost(args[args.index('--host') + 1])        
     if '--ports' in args:
         index = args.index('--ports') + 1
-        if index == len(args) or not re.search('^\d{1,4}-\d{1,4}$', args[index]):
+        if index == len(args):
             print("Invalid ports specified, setting to default (1-65535)")
         else:
-            limits = [int(i) for i in args[index].split('-')]
-            if limits[0] >= limits[1] or any([i not in range(1, 65536) for i in limits]):
+            try:
+                ports = _parsePorts(args[index])
+            except ValueError:
                 print("Invalid ports specified, setting to default (1-65535)")
-            else:
-                ports = range(limits[0], limits[1]+1)
     return(host, ports)
 
-def scan(host, ports):
+def _scan(host, ports):
     socket.setdefaulttimeout(0.3)
     openPorts=[]
-    tw = twister()
+    tw = _twister()
     print('scanning', end = ' ', flush=True)
     for port in ports:
         with socket.socket() as s:
@@ -61,16 +73,35 @@ def scan(host, ports):
     result = ''
     if len(openPorts):
         result = "Port{} {} {}".format('s' if len(openPorts)%1 == 1 else '',
-        ' '.join((str(p) for p in openPorts)), "are" if len(openPorts) > 1 else "is")
+        ', '.join((str(p) for p in openPorts)), "are" if len(openPorts) > 1 else "is")
     else:
         result = "No ports are"
     print(f' \n{result} open')
 
 
 
+def scan(host, portsString = ''):
+    try:
+        host = _parseHost(host)
+    except Exception as er:
+        print(er)
+        return
+    ports = range(1, 65536)
+    if portsString:
+        try:
+            ports = _parsePorts(portsString)
+        except:
+            print ("Invalid ports specified, setting to default (1-65535)")
+    _scan(host, ports)
+
+        
+
+
+
+
 if __name__ == "__main__":
     try:
-        scan(*parseArguments(sys.argv[1:]))
+        _scan(*_parseArguments(sys.argv[1:]))
     except Exception as er:
         print(er)
             
