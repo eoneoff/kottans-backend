6699d014525952e657a1a2cf9e00af1bcbe13d96
eoneoff <eoneoff@gmail.com>
2019-10-10 07:24:55 +0300

    Node class added to nodejs data structures

---
+++ task_data_structures/nodejs/data_structures/node.js
@@ -0,0 +1,22 @@
+module.exports.Node = class Node {
+    constructor(value, next) {
+        this._value = value;
+        this._next = next;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(value) {
+        this._value = value;
+    }
+
+    get next() {
+        return this._next;
+    }
+
+    set next(next) {
+        this._next = next;
+    }
+};
