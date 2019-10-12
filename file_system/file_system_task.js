5e37004ffa24494f6d66774b5efc8ef717eeb22f
eoneoff <eoneoff@gmail.com>
2019-10-12 20:08:52 +0300

    file system task completed

---
+++ file_system/file_system_task.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+if (fs.existsSync("counter.txt")) {
+    const content = fs.readFileSync("counter.txt");
+    if (content == parseInt(content)) {
+        fs.writeFileSync("counter.txt", (+content + 1).toString());
+        process.exit(0);
+    } else {
+        process.exit(1);
+    }
+} else {
+    fs.writeFileSync("counter.txt", "1");
+    process.exit(0);
+}
