e101c559a957ab1fc3d9ab38cafa55656b794740
eoneoff <eoneoff@gmail.com>
2019-12-17 18:07:36 +0200

    file system task.js corrections

--- file_system/file_system_task.js
+++ file_system/file_system_task.js
@@ -1,14 +1,14 @@
-const fs = require("fs");
+const fs = require('fs');
 
-if (fs.existsSync("counter.txt")) {
-    const content = fs.readFileSync("counter.txt");
+if (fs.existsSync('counter.txt')) {
+    const content = fs.readFileSync('counter.txt');
     if (content == parseInt(content)) {
-        fs.writeFileSync("counter.txt", (+content + 1).toString());
+        fs.writeFileSync('counter.txt', (+content + 1).toString());
         process.exit(0);
     } else {
         process.exit(1);
     }
 } else {
-    fs.writeFileSync("counter.txt", "1");
+    fs.writeFileSync('counter.txt', '1');
     process.exit(0);
 }
